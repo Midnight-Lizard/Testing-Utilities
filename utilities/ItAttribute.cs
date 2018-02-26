cd6f42da86dfe0383b864f0020087959d346ebdd
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-02-26 10:41:08 +0300

    MidnightLizard.Testing.Utilities proj added

---
+++ utilities/ItAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace MidnightLizard.Testing.Utilities
+{
+    /// <summary>
+    /// Xunit Fact with custom test name processing
+    /// </summary>
+    public class ItAttribute : FactAttribute
+    {
+        public ItAttribute(string it, [CallerMemberName]string should = "should work") : base()
+        {
+            //DisplayName = Regex.Replace(Regex.Replace(Regex.Replace(
+            //    displayName, @"((?:[A-Z])+[^A-Z]*)", "$1 "), "_", " "), "  ", " ")
+            //    .Trim().ToLower();
+
+            //DisplayName = string.Join(" ", Regex
+            //    .Split(displayName, @"_|((?:[A-Z])+[^A-Z_]*)")
+            //    .Select(x => x.Trim()));
+
+            DisplayName = $"{it} - {should}".Replace("__", ".").Replace("_", " ");
+        }
+
+    }
+}
---
+++ utilities/ItsAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace MidnightLizard.Testing.Utilities
+{
+    /// <summary>
+    /// Xunit Theory with custom test name processing
+    /// </summary>
+    public class ItsAttribute : TheoryAttribute
+    {
+        public ItsAttribute(string it, [CallerMemberName]string should = "") : base()
+        {
+            DisplayName = $"{it} - {should} ".Replace("__", ".").Replace("_", " ");
+        }
+
+    }
+}
